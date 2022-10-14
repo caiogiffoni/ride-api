d8f203a8803e3565a30fcbedca534e71f981a323
Caio Giffoni <caio_cgf@hotmail.com>
2022-10-14 17:01:03 -0300

    :sparkles: created: Rides resolver

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { ApolloServer } from "apollo-server";
 import { context } from "./context";
 import { UserResolver } from "./User/UserResolver";
 import { buildSchema } from "type-graphql";
+import { RidesResolver } from "./Rides/RidesResolver";
 
 const app = async () => {
-  const schema = await buildSchema({ resolvers: [UserResolver] });
+  const schema = await buildSchema({
+    resolvers: [UserResolver, RidesResolver],
+  });
 
   new ApolloServer({ schema, context: context }).listen({ port: 3000 }, () =>
     console.log(`Server is running 🚀`)
