b6db7b5bfd96ccffd61f8abc93f9b7b3a152169a
Caio Giffoni <caio_cgf@hotmail.com>
2022-10-16 18:06:01 -0300

    :sparkles: added jest config and dir created

---
+++ src/tests/users/user.spec.ts
@@ -0,0 +1,8 @@
+import request from "supertest";
+import { describe, expect, test, beforeAll, afterAll } from "@jest/globals";
+
+describe("create user", () => {
+  test("Create an user", async () => {
+    expect("201").toEqual("201");
+  });
+});
